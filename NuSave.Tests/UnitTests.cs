20cfb6c6bab7f1c0c6d3b42d7594df33f763dd0b
Elias Bouassaba <ebouassaba@gmail.com>
2021-06-20 13:25:21 +0100

    Unit test fixes

--- NuSave.Tests/UnitTests.cs
+++ NuSave.Tests/UnitTests.cs
@@ -1,6 +1,8 @@
 namespace NuSave.Tests
 {
   using System;
+  using System.Collections.Generic;
+  using System.Linq;
   using Microsoft.VisualStudio.TestTools.UnitTesting;
   using Core;
 
@@ -10,10 +12,20 @@ namespace NuSave.Tests
     [TestMethod]
     public void TestDependencyResolver()
     {
-      // var downloader = new DependencyResolver(null, null, Environment.CurrentDirectory, "System.Collections", "4.3.0");
-      // downloader.ResolveDependencies();
-      // var deps = downloader.GetDependencies();
-      // Assert.IsTrue(deps.Count > 0);
+      const string source = "https://api.nuget.org/v3/index.json";
+
+      var cache = new Cache(Environment.CurrentDirectory);
+
+      var dependencyResolver = new DependencyResolver(new DependencyResolver.Options
+      {
+        Source = source,
+        TargetFrameworks = new List<string> {".NETStandard@1.0", ".NETStandard@1.3"},
+        AllowPreRelease = true,
+        AllowUnlisted = false,
+      }, cache);
+      dependencyResolver.ResolveByIdAndVersion("Newtonsoft.Json", "12.0.3");
+
+      Assert.IsTrue(dependencyResolver.Dependencies.Any());
     }
   }
 }
